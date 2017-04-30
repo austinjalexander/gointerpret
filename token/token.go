b248dd25b077c67c8708ba5caa8b64044d2da3fb
Austin J. Alexander <austinjalexander@gmail.com>
2017-04-30 13:03:46 -0700

    Add token

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+// TokenType distinguishes between Tokens.
+type TokenType string
+
+// Token represents an object that has been lexed from source code.
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
